0b1fc548d32db16021fb078601781c99ceb653c4
jwill387 <98768301+jwill387@users.noreply.github.com>
2022-03-03 15:53:42 -0500

    Set Up

---
+++ stocks.R
@@ -0,0 +1,23 @@
+library(fpp3)
+
+# Read data
+stocks <- read.csv("nyse_stocks.csv")
+
+#Convert to tsibble()
+stocks$date <- as.Date(stocks$date)
+stocks <- tsibble(stocks, index = date, key = symbol)
+
+# Plotting 1 stock
+selected_stock <- "AAPL"
+
+stocks %>%
+  filter(symbol == selected_stock) %>%
+  autoplot(volume) +
+  labs(title = selected_stock)
+
+# Plotting multiple stocks
+selected_stocks <- c("GOOG", "AAPL")
+
+stocks %>%
+  filter(symbol %in% selected_stocks) %>%
+  autoplot(volume)
